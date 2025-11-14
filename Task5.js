ad3b7aac2b35a356dd9aaebb04fa5d6ac0962c35
Keshomigha <keshomighasajeeve@gmail.com>
2025-11-14 11:38:57 +0530

    modified files

---
+++ Task5.js
@@ -0,0 +1,15 @@
+//Task 5: Challenge Activity
+
+const products = [
+    {name: "Keyboard", price: 2500, qty: 2},
+    {name: "Mouse", price: 1500, qty: 3},
+    {name: "Monitor", price: 22000, qty: 1}
+];
+
+// Calculate total value using reduce
+const totalValue = products.reduce((total, product) => {
+    return total + (product.price * product.qty);
+}, 0);
+
+console.log("Products:", products);
+console.log("Total value of all products: Rs.", totalValue);
